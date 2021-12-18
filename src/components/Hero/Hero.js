eb74fb85cf872690e746b94f966549dfd9ca3403
raquelgrogan <raquelgrogan@gmail.com>
2021-12-18 16:57:37 -0500

    Adding hero headers and text, along with an email button

--- src/components/Hero/Hero.js
+++ src/components/Hero/Hero.js
@@ -5,9 +5,17 @@ import Button from '../../styles/GlobalComponents/Button';
 import { LeftSection } from './HeroStyles';
 
 const Hero = (props) => (
-  <div>
-    Hero
-  </div>
+  <Section row nopadding>
+    <LeftSection>
+      <SectionTitle main center>
+        Welcome To <br/> Jessica's Portfolio
+      </SectionTitle>
+      <SectionText>
+        This portfilio contains my 3D animations, Graphic Design, and Motion Graphics I've created throughout the years.
+      </SectionText>
+      <Button onClick={() => location.href = 'mailto:raquelgrogan@gmail.com'}>Email Me</Button>
+    </LeftSection>
+  </Section>
 );
 
 export default Hero;
